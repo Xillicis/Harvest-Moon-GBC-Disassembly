83f4e1fd6aab4f19888817e3c0d3901d3b63f0e1
Xillicis <thehorrorguy@ymail.com>
2025-07-19 22:00:15 -0600

    Broke out the text and got more data.

---
+++ text/farm.asm
@@ -0,0 +1,34 @@
+TooLateText1::
+    db $00, $00, $00, $00, $00, $00, $78, $70, $00, $00
+    db "Oops! It's       "
+    db "already 6AM.   ▽"
+    prompt
+    db $00, $00, $00, $00, $00, $00, $78, $70, $00, $00
+    db "I better go home"
+    db "and have       ▽"
+    prompt
+    db $00, $00, $00, $00, $00, $00, $78, $70, $00, $00
+    db "something to    "
+    db "eat.            "
+    done
+
+TooLateText2::
+    db $00, $00, $00, $00, $00, $00, $78, $70, $00, $00
+    db "Oh My! It's      "
+    db "already 6AM.   ▽"
+    prompt
+    db $00, $00, $00, $00, $00, $00, $78, $70, $00, $00
+    db "I better get    "
+    db "going home!     "
+    done
+
+ShippingBoxText::
+    db $00, $00, $00, $00, $00, $00, $78, $70, $00, $00
+    db " [Shipping Box] "
+    db "Pick up is at  ▽"
+    prompt
+    db $00, $00, $00, $00, $00, $00, $78, $70, $00, $00
+    db "5PM everyday.   "
+    db "     From Trader"
+    done
+
